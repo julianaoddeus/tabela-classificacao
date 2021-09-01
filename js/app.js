c133eb90968210fb8df129675489d71f7bb491f9
Juliana Oliveira <julianaacademi@gmail.com>
2021-08-31 22:34:20 -0300

    criado funções para acrescentar a contagem de cada ação, desafio finalizado

--- js/app.js
+++ js/app.js
@@ -5,18 +5,24 @@ let btvitoriaJogador1 = document.querySelector('#vitoriaJogador1');
 btvitoriaJogador1.onclick = () => {
     pontoJogador1 += CalcularPontos("vitoria");
     AtualizaTabelaPontos("jogador1");
+    SomaVitorias();
+    
 };
+
+
 
 let btempateJogador1 = document.querySelector('#empateJogador1');
 btempateJogador1.onclick = () => {
     pontoJogador1 += CalcularPontos("empate");
     AtualizaTabelaPontos("jogador1");
+    SomaEmpates();
 };
 
 let btderrotaJogador1 = document.querySelector('#derrotaJogador1');
 btderrotaJogador1.onclick = () => {
     pontoJogador1 += CalcularPontos("derrota");
     AtualizaTabelaPontos("jogador2");
+    SomaDerrotas();
 }
 
 
@@ -30,6 +36,7 @@ let btempateJogador2 = document.querySelector('#empateJogador2');
 btempateJogador2.onclick = () => {
     pontoJogador2 += CalcularPontos("empate");
     AtualizaTabelaPontos("jogador2");
+   
 }
 
 let btderrotaJogador2 = document.querySelector('#derrotaJogador2');
@@ -38,7 +45,27 @@ btderrotaJogador2.onclick = () => {
     AtualizaTabelaPontos("jogador2");
 }
 
+
+
 // Funções para atualizar a contagem de pontos
+let vitoria = 0, empate = 0, derrota = 0;
+function SomaVitorias() {
+    vitoria += 1;
+    document.querySelector('.vitorias-jogador1').innerHTML = vitoria;
+}
+
+
+function SomaEmpates() {
+    empate += 1;
+    document.querySelector('.empates-jogador1').innerHTML = empate;
+}
+
+function SomaDerrotas() {
+    derrota += 1;
+    document.querySelector('.derrotas-jogador1').innerHTML = derrota;
+}
+
+
 function AtualizaTabelaPontos(time) {
     if(time == "jogador1") {
         document.querySelector(".pontos-jogador1").innerText = pontoJogador1;
